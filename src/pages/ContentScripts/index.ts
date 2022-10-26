e1e343ac9f4158228d5f3d93721319a07e6b8461
Nir Geller <gnir.work@gmail.com>
2022-10-27 00:30:07 +0300

    Added basic support for folder download.

--- src/pages/ContentScripts/index.ts
+++ src/pages/ContentScripts/index.ts
@@ -1,5 +1,6 @@
 function add_download_icon(row: Element) {
     const is_file = !!row.querySelector('svg[aria-label=File]');
+    const is_folder = !!row.querySelector('svg[aria-label=Directory]');
     if (is_file) {
         row.children[0].setAttribute("style", "width: 32px")
 
@@ -7,7 +8,7 @@ function add_download_icon(row: Element) {
         download_icon.style.cursor = "pointer";
         download_icon.style.marginRight = "0.5em";
         download_icon.textContent = "⇩"
-        download_icon.addEventListener("click" ,() => {
+        download_icon.addEventListener("click", () => {
             const file_link = row.querySelector('a')?.href
             if (file_link) {
                 chrome.runtime.sendMessage({message: file_link})
@@ -16,7 +17,27 @@ function add_download_icon(row: Element) {
 
         row.insertAdjacentElement("afterbegin", download_icon);
     }
+    if (is_folder) {
+        row.children[0].setAttribute("style", "width: 32px")
+
+        const download_icon = document.createElement("span")
+        download_icon.style.cursor = "pointer";
+        download_icon.style.marginRight = "0.5em";
+        download_icon.textContent = "⇩"
+        download_icon.addEventListener("click", () => {
+            const folder_link = row.querySelector('a')?.href
+            if (folder_link) {
+                window.open(
+                    `https://download-directory.github.io/?url=${encodeURIComponent(folder_link)}`,
+                    'blank'
+
+                )
+            }
+        })
+        row.insertAdjacentElement("afterbegin", download_icon);
+    }
 }
+
 
 function main() {
     const rows = document.querySelectorAll('div.Details div.js-navigation-item');
