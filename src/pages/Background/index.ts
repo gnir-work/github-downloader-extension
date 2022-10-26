a7363c18c0c82a78ab2730082d4c4f1c01292f53
Nir Geller <gnir.work@gmail.com>
2022-10-26 12:54:35 +0300

    Copied code from previous version. Added required permissions.

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,1 +1,16 @@
-console.log('This is the background page.');
+function downloadUrl(url: string, filename: string) {
+    chrome.downloads.download({
+        url,
+        filename
+    })
+}
+
+chrome.runtime.onMessage.addListener(({ message }) => {
+    console.log(`Received the following href ${message}, starting to download...`);
+    const raw_url = message.replace("/blob/", "/raw/");
+    let file_name = raw_url.split("/").at(-1);
+    if (file_name.startsWith('.')) {
+        file_name = file_name.substring(1)
+    }
+    downloadUrl(raw_url, file_name);
+})
--- src/pages/ContentScripts/index.ts
+++ src/pages/ContentScripts/index.ts
@@ -1,1 +1,30 @@
-console.log('Content script works!');
+function add_download_icon(row: Element) {
+    const is_file = !!row.querySelector('svg[aria-label=File]');
+    if (is_file) {
+        row.children[0].setAttribute("style", "width: 32px")
+
+        const download_icon = document.createElement("span")
+        download_icon.style.cursor = "pointer";
+        download_icon.style.marginRight = "0.5em";
+        download_icon.textContent = "⇩"
+        download_icon.addEventListener("click" ,() => {
+            const file_link = row.querySelector('a')?.href
+            if (file_link) {
+                chrome.runtime.sendMessage({message: file_link})
+            }
+        })
+
+        row.insertAdjacentElement("afterbegin", download_icon);
+    }
+}
+
+function main() {
+    const rows = document.querySelectorAll('div.Details div.js-navigation-item');
+    if (rows) {
+        for (const row of Array.from(rows)) {
+            add_download_icon(row);
+        }
+    }
+}
+
+window.addEventListener("load", main)
