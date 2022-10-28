ece91c187f1e2e27351fca0204d6d0801ab5d524
Nir Geller <gnir.work@gmail.com>
2022-10-28 17:01:11 +0300

    Fix linting problem.

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,7 +1,7 @@
 import { GithubBlobData } from './types';
 import { BLOB_IDENTIFIER, RAW_IDENTIFIER } from './consts';
 
-function parse_url_to_blob_data(url: string): GithubBlobData {
+function parseUrlToBlobData(url: string): GithubBlobData {
   const rawUrl = url.replace(BLOB_IDENTIFIER, RAW_IDENTIFIER);
   let fileName = rawUrl.split('/').at(-1);
   if (fileName === undefined) {
@@ -19,7 +19,7 @@ function parse_url_to_blob_data(url: string): GithubBlobData {
 chrome.runtime.onMessage.addListener(({ message: url }) => {
   // eslint-disable-next-line no-console
   console.log(`Received the following href ${url}, starting to download...`);
-  const blobData = parse_url_to_blob_data(url);
+  const blobData = parseUrlToBlobData(url);
   chrome.downloads.download({
     url: blobData.url,
     filename: blobData.blobName,
