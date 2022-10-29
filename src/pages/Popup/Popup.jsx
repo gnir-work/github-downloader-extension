d575d7c7578ef986920cccc4c80b33fa0e3218ae
Nir Geller <gnir.work@gmail.com>
2022-10-29 22:16:59 +0300

    Save the token in extension storage.

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Popup.css';
 
 function Popup() {
+  const [token, setToken] = useState('');
+
+  const handleTokenChange = (event) => {
+    setToken(event.target.value);
+  };
+
+  useEffect(() => {
+    chrome.storage.sync.get(['token'], (data) => {
+      setToken(data.token);
+    });
+  }, [setToken]);
+
+  useEffect(() => {
+    chrome.storage.sync.set({ token });
+  }, [token]);
+
   return (
     <div className="App">
-      <h2> hello </h2>
+      <input type="text" value={token} onChange={handleTokenChange} />
+
     </div>
   );
 }
